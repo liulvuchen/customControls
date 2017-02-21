e745c295b02394f931685d9d89442d2b0dcd3349
longen0707 <2867058917@qq.com>
2017-02-21 17:02:05 +0800

    三级联动自定义

---
+++ CustomControlsDemo/app/src/main/java/com/lyl/customcontrolsdemo/bean/PickerViewData.java
@@ -0,0 +1,23 @@
+package com.lyl.customcontrolsdemo.bean;
+
+import com.lyl.pickerview.model.IPickerViewData;
+
+/**
+ * Created by Administrator on 2016/7/13.
+ */
+public class PickerViewData implements IPickerViewData {
+    private String content;
+
+    public PickerViewData(String content) {
+        this.content = content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String getPickerViewText() {
+        return content;
+    }
+}
---
+++ CustomControlsDemo/pickerview/src/main/java/com/lyl/pickerview/listener/OnItemSelectedListener.java
@@ -0,0 +1,6 @@
+package com.lyl.pickerview.listener;
+
+
+public interface OnItemSelectedListener {
+    void onItemSelected(int index);
+}
